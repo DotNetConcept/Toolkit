3119c67b4eea84f22bf96736a61f722f025afaf9
Cyril MARTY <cyril.marty@dotnetconcept.fr>
2019-11-08 12:29:54 +0100

    MAJ Packages + AddScopedOptions

---
+++ src/Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNetConcept.Toolkit.Extensions
+{
+    using JetBrains.Annotations;
+
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Options;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddScopedOptions<TOptions>([NotNull] this IServiceCollection services)
+            where TOptions : class, new()
+        {
+            services.AddScoped(sp => sp.GetService<IOptionsSnapshot<TOptions>>().Value);
+        }
+    }
+}
